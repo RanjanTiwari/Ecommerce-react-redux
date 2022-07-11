266a1df1aee2a528b620bfd3750606ff8316f4a9
Ranjantiwari <Ranjant13@gmail.com>
2022-07-11 14:06:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+import CardsDetails from './components/CardsDetails';
+import Cards from './components/Cards';
+import {Routes,Route} from "react-router-dom";
+import Footer from './components/Footer'
+
+
+function App() {
+  return (
+  <>
+   <Header />
+   <Routes>
+     <Route path='/' element={<Cards />} />
+     <Route path='/cart/:id' element={<CardsDetails />} />
+   </Routes>
+  <Footer/>
+   
+  </>
+  );
+}
+
+export default App;
